214a3b6be9d98bbb6919ec1f734903dc3a4033ed
eoghanmccarthy <eoghanmccarthy@outlook.com>
2016-09-04 23:56:48 +0100

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<div class="row">
+  <footer class="col-md-12">
+    <p>2016</p>
+  </footer>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<header class="row">
+  <div class="col-md-9">
+    <img class="img-fluid" src="images/theseboys-logo.png" alt="theseboys">
+  </div>
+  <div class="col-md-3">
+    <p></p>
+  </div>
+</header>
