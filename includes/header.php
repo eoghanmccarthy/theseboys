d38601e7722aa6580a4178925b727ed49a583fce
eoghanmccarthy <eoghanmccarthy@outlook.com>
2016-11-05 15:10:27 +0000

    delete header

--- includes/header.php
+++
@@ -1,8 +0,0 @@
-<header class="row">
-  <div class="col-md-9">
-    <img class="img-fluid" src="images/theseboys-logo.png" alt="theseboys">
-  </div>
-  <div class="col-md-3">
-    <p></p>
-  </div>
-</header>
