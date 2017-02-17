157b82a65ab9d3c7a2b9700a1b261b5d8bfa9e5a
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-17 23:38:29 +0000

    these

---
+++ partials/header.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <header class="col-xs-12 header">
+        <div class="header">
+            <h1><span>theseboys</span></h1>
+            <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter icon-twitter" aria-hidden="true"></i></a>
+            <div class="col-xs-12 sub-header">
+                <h2>everything (we do) is a work-in-progress</h2>
+            </div>
+        </div>
+    </header>
+</div>
