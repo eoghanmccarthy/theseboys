aa91aa3d785c84f66b5033b6074c9c363ca0dea1
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-17 23:38:00 +0000

    refactor

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+
+// Gulp dependencies go here
+gulp.task('default', () => {
+    return gulp.src('src/js/es6/**/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('src/js/dist'));
+});
+
+gulp.task('sass', () => {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', () => {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+});
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>theseboys</title>
+        <?php include 'partials/meta.php';?>
+        <?php include 'partials/scripts.php';?>
+    </head>
+
+    <body>
+        <div class="container-fluid">
+            <!-- Header -->
+            <?php include 'partials/header.php';?>
+            <main>
+                <section>
+                </section>
+            </main>
+            <!-- Footer -->
+            <?php include 'partials/footer.php';?>
+        </div>
+        <?php include 'partials/scripts-js.php';?>
+    </body>
+
+</html>
