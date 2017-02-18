d858ed8833dea3affe23d319683cfd7a30169031
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-18 14:41:46 +0000

    sass

--- partials/header.php
+++ partials/header.php
@@ -1,11 +1,11 @@
 <div class="row">
-    <header class="col-xs-12 header">
-        <div class="header">
-            <h1><span>theseboys</span></h1>
-            <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter icon-twitter" aria-hidden="true"></i></a>
-            <div class="col-xs-12 sub-header">
+    <div class="col-md-8 offset-md-2">
+        <header>
+            <div class="header">
+                <h1><span>theseboys</span></h1>
+                <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter icon-twitter" aria-hidden="true"></i></a>
                 <h2>everything (we do) is a work-in-progress</h2>
             </div>
-        </div>
-    </header>
+        </header>
+    </div>
 </div>
