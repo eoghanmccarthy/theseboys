85c0e98a900855091547e17147c3094d164284ad
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-18 15:30:03 +0000

    center

--- partials/header.php
+++ partials/header.php
@@ -2,6 +2,9 @@
     <div class="col-md-8 offset-md-2">
         <header>
             <div class="header">
+                <div class="header__logo">
+                    <?php include 'partials/logo.php';?>
+                </div>
                 <h1><span>theseboys</span></h1>
                 <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter icon-twitter" aria-hidden="true"></i></a>
                 <h2>everything (we do) is a work-in-progress</h2>
