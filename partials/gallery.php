1a142f720791247b14beeebf6b76ba66a589badb
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-19 15:06:07 +0000

    media

--- partials/gallery.php
+++ partials/gallery.php
@@ -3,22 +3,22 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <div id="gallery" class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-6 no-padding">
                         <figure class="media">
                             <img class="media__content" src="img/img_3564.jpg" onclick="topSlide(1)">
                         </figure>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 no-padding">
                         <figure class="media">
                             <img class="media__content" src="img/img_3568.jpg" onclick="topSlide(2)">
                         </figure>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 no-padding">
                         <figure class="media">
                             <img class="media__content" src="img/img_4006.jpg" onclick="topSlide(3)">
                         </figure>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-6 no-padding">
                         <figure class="media">
                             <img class="media__content" src="img/img_4013.jpg" onclick="topSlide(4)">
                         </figure>
