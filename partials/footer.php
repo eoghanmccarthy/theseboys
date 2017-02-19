531ff9baca0b206473f604474176cb256c14c5c1
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-19 14:16:22 +0000

    social

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
-<div class="row">
-    <footer class="col-md-12">
-        <p>2016</p>
-    </footer>
-</div>
+<footer>
+    <div class="social social__twitter">
+        <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+    </div>
+</footer>
--- partials/header.php
+++ partials/header.php
@@ -1,14 +1,18 @@
-<div class="row">
-    <div class="col-md-8 offset-md-2">
-        <header>
-            <div class="header">
-                <div class="header__logo">
-                    <?php include 'partials/logo.php';?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <header>
+                <div class="header">
+                    <div class="header__logo">
+                        <?php include 'partials/logo.php';?>
+                    </div>
+                    <h1>theseboys</h1>
+                    <div class="social social__twitter">
+                        <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </div>
+                    <h2>everything (we do) is a work-in-progress</h2>
                 </div>
-                <h1>theseboys</h1>
-                <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter icon-twitter" aria-hidden="true"></i></a>
-                <h2>everything (we do) is a work-in-progress</h2>
-            </div>
-        </header>
+            </header>
+        </div>
     </div>
 </div>
