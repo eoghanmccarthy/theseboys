c37cc5c5c2c7e6412ad39c4d92ac38a1efadc9af
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-19 19:00:37 +0000

    php

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,4 @@
 <footer>
-    <div class="social social__twitter">
-        <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-    </div>
+    <?php include 'partials/logo.php';?>
+    <?php include 'partials/social.php';?>
 </footer>
--- partials/header.php
+++ partials/header.php
@@ -4,9 +4,7 @@
             <header>
                 <?php include 'partials/logo.php';?>
                 <h1>theseboys</h1>
-                <div class="social social__twitter">
-                    <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                </div>
+                <?php include 'partials/social.php';?>
                 <h2>everything (we do) is a work-in-progress</h2>
             </header>
         </div>
