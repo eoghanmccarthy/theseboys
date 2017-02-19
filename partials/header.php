68b9a1ab8ff64822aca226ed185bf0be37005fe0
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-19 17:39:20 +0000

    logo

--- partials/header.php
+++ partials/header.php
@@ -1,18 +1,14 @@
-<header>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 offset-md-2">
-                <div class="header">
-                    <div class="logo">
-                        <?php include 'partials/logo.php';?>
-                    </div>
-                    <h1>theseboys</h1>
-                    <div class="social social__twitter">
-                        <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                    </div>
-                    <h2>everything (we do) is a work-in-progress</h2>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 offset-md-2">
+            <header>
+                <?php include 'partials/logo.php';?>
+                <h1>theseboys</h1>
+                <div class="social social__twitter">
+                    <a href="http://www.twitter.com/theseboys" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
                 </div>
-            </div>
+                <h2>everything (we do) is a work-in-progress</h2>
+            </header>
         </div>
     </div>
-</header>
+</div>
