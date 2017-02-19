8c39ea017aaea237b76ffef52bbbfcfeb0be7029
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-02-19 14:56:02 +0000

    gallery

--- index.php
+++ index.php
@@ -12,32 +12,9 @@
             <!-- Header -->
             <?php include 'partials/header.php';?>
             <main>
-                <section class="band band__gallery">
-                    <div class="container">
-                        <div id="gallery" class="row">
-                            <div class="col-md-6">
-                                <figure class="media">
-                                    <img class="media__content" src="img/img_3564.jpg" onclick="topSlide(1)">
-                                </figure>
-                            </div>
-                            <div class="col-md-6">
-                                <figure class="media">
-                                    <img class="media__content" src="img/img_3568.jpg" onclick="topSlide(2)">
-                                </figure>
-                            </div>
-                            <div class="col-md-6">
-                                <figure class="media">
-                                    <img class="media__content" src="img/img_4006.jpg" onclick="topSlide(3)">
-                                </figure>
-                            </div>
-                            <div class="col-md-6">
-                                <figure class="media">
-                                    <img class="media__content" src="img/img_4013.jpg" onclick="topSlide(4)">
-                                </figure>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <div class="band">
+                    <?php include 'partials/gallery.php';?>
+                </div>
             </main>
             <!-- Footer -->
             <?php include 'partials/footer.php';?>
---
+++ partials/gallery.php
@@ -0,0 +1,30 @@
+<section class="band__gallery">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div id="gallery" class="row">
+                    <div class="col-md-6">
+                        <figure class="media">
+                            <img class="media__content" src="img/img_3564.jpg" onclick="topSlide(1)">
+                        </figure>
+                    </div>
+                    <div class="col-md-6">
+                        <figure class="media">
+                            <img class="media__content" src="img/img_3568.jpg" onclick="topSlide(2)">
+                        </figure>
+                    </div>
+                    <div class="col-md-6">
+                        <figure class="media">
+                            <img class="media__content" src="img/img_4006.jpg" onclick="topSlide(3)">
+                        </figure>
+                    </div>
+                    <div class="col-md-6">
+                        <figure class="media">
+                            <img class="media__content" src="img/img_4013.jpg" onclick="topSlide(4)">
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
