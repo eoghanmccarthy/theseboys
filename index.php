be0911c1c7109c18b2960d78793131c4441fe226
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-04-08 13:16:42 +0100

    react

--- index.php
+++ index.php
@@ -15,14 +15,21 @@
 			<main>
 				<div id="app"></div>
 				<script type="text/jsx">
-					let Player = React.createClass({
-						render: function() { return (
-							<h2>hey</h2>
-						)}
-					});
+					import React from 'react';
+					import ReactDOM from 'react-dom';
+
 					ReactDOM.render(
-						<Player />, document.getElementById('app')
+					<h1>Hello, world!</h1>,
+					document.getElementById('app')
 					);
+					// let Player = React.createClass({
+					// 	render: function() { return (
+					// 		<h2>hey</h2>
+					// 	)}
+					// });
+					// ReactDOM.render(
+					// 	<Player />, document.getElementById('app')
+					// );
 				</script>
 			</main>
 			<!-- Footer -->
