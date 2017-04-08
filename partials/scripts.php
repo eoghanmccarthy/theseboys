e3a7adbc8fc59ddf823353a5a3f1aca2020116dd
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-04-08 13:00:02 +0100

    js

--- partials/scripts.php
+++ partials/scripts.php
@@ -1,14 +1,4 @@
-<!-- Latest compiled and minified Bootstrap CSS -->
+<!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
-
-<!-- Latest compiled and minified Bootstrap JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
-
-<!-- Latest compiled and minified JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-
-<!-- Font Awesome -->
-<script src="https://use.fontawesome.com/e6985d2f93.js"></script>
-
 <!-- Custom CSS -->
 <link href="src/css/main.css" rel="stylesheet" type="text/css" />
