2f9e063473598951fe661ee70f72f41062e92a1f
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-04-08 12:59:42 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,19 @@ const babel = require('gulp-babel');
 
 // Gulp dependencies go here
 gulp.task('default', () => {
-    return gulp.src('src/js/es6/**/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('src/js/dist'));
+	return gulp.src('src/js/es6/**/*.js')
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(gulp.dest('src/js/dist'));
 });
 
 gulp.task('sass', () => {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src/css'));
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('src/css'));
 });
 
 gulp.task('watch', () => {
-    gulp.watch('src/scss/**/*.scss', ['sass']);
+	gulp.watch('src/scss/**/*.scss', ['sass']);
 });
--- index.php
+++ index.php
@@ -1,23 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <title>theseboys</title>
-        <?php include 'partials/meta.php';?>
-        <?php include 'partials/scripts.php';?>
-    </head>
+	<head>
+		<title>theseboys</title>
+		<?php include 'partials/meta.php';?>
+		<?php include 'partials/scripts.php';?>
+	</head>
 
-    <body>
-        <div class="container-fluid">
-            <!-- Header -->
-            <?php include 'partials/header.php';?>
-            <main>
-                <?php include 'partials/audio.php';?>
-            </main>
-            <!-- Footer -->
-            <?php include 'partials/footer.php';?>
-        </div>
-        <?php include 'partials/scripts-js.php';?>
-    </body>
+	<body>
+		<div class="container-fluid">
+			<!-- Header -->
+			<?php include 'partials/header.php';?>
+			<!-- Main -->
+			<main>
+				<div id="app"></div>
+				<script type="text/jsx">
+					let Player = React.createClass({
+						render: function() { return (
+							<h2>hey</h2>
+						)}
+					});
+					ReactDOM.render(
+						<Player />, document.getElementById('app')
+					);
+				</script>
+			</main>
+			<!-- Footer -->
+			<?php include 'partials/footer.php';?>
+		</div>
+		<?php include 'partials/scripts-js.php';?>
+	</body>
 
 </html>
