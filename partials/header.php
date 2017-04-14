28637e56653d3d394840e0c3541563a2bc8eea52
eoghanmccarthy <eoghanmccarthy@outlook.com>
2017-04-14 21:59:58 +0100

    logo

--- partials/header.php
+++ partials/header.php
@@ -1,11 +1,11 @@
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 offset-md-2">
-            <header>
-                <?php include 'partials/logo.php';?>
-                <h1>theseboys</h1>
-                <?php include 'partials/social.php';?>
-            </header>
-        </div>
-    </div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2">
+			<header>
+				<?php include 'partials/logo.php';?>
+				<h1>theseboys</h1>
+				<?php include 'partials/social.php';?>
+			</header>
+		</div>
+	</div>
 </div>
