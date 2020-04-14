4568b5e8ea5aa2c5b831c8edc775f408c74e9c0e
Eoghan Mccarthy <eoghanmccarthy@outlook.com>
2020-04-14 01:43:26 +0100

    add module transition

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 
 import "./styles.scss";
 
@@ -10,14 +11,26 @@ import StepSequencer from "features/stepSequencer";
 const modules = ["step sequencer"];
 
 const Home = () => {
+  const [props, set, stop] = useSpring(() => ({ x: 0 }));
+
+  console.log(props);
+
   return (
     <TransportProvider>
       <main className={"me__content"}>
+        <button onClick={() => set({ x: 1 })}>next</button>
         <div className={"console"}>
           <Master />
           <div className={"modules"}>
             <ModulesNav />
-            <StepSequencer />
+            <animated.div
+              style={{
+                display: "flex",
+                transform: props.x.interpolate(v => `translateX(${v * 100}%)`)
+              }}
+            >
+              <StepSequencer />
+            </animated.div>
           </div>
         </div>
       </main>
