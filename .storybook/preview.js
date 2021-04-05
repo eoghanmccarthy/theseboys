8472851783500123835c5ca2c61f1bbea5ec29a6
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2021-04-05 12:47:20 +0900

    Add storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import '!style-loader!css-loader!../app/styles.css';
+import '!style-loader!css-loader!./styles.css';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
