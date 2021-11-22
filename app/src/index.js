a4f35828df8cd5e0311dadb328084a50dc008711
Eoghan McCarthy <eoghanmccarthy@outlook.com>
2021-11-22 09:41:05 +0000

    remove routes directory

--- app/src/index.js
+++ app/src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import './styles.css';
+import Home from './pages/home';
+import Studio from './pages/studio';
+import { MasterProvider } from 'components/master';
 
-import { useGetAuth } from './utils/api';
-
-import Routes from './routes';
-
-const App = () => {
-  return <Routes />;
-  // const auth = useGetAuth({ enabled: window.location.hostname !== 'localhost' });
-  // return window.location.hostname === 'localhost' || auth.isSuccess ? <Routes /> : null;
-};
+const App = () => (
+  <div className={'me'}>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        exact
+        path="/studio"
+        element={
+          <MasterProvider>
+            <Studio />
+          </MasterProvider>
+        }
+      />
+      <Route path={'*'} element={<Home />} />
+    </Routes>
+  </div>
+);
 
 export default App;
